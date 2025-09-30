6e28bfa8a958a93847baf445fdf7258f96701737
DanielBoigk <dn.boigk@gmail.com>
2025-10-01 01:25:00 +0200

    add L1 norm

--- src/Galerkin/Ferrite/SolverState/SolverState.jl
+++ src/Galerkin/Ferrite/SolverState/SolverState.jl
@@ -8,5 +8,12 @@ mutable struct FerriteSolverState <: AbstractGalerkinSolver
     Σ::AbstractVector # Singular values of K
     d # metric we use
     ∂d # derivative of the metric
+    down # Projection from force vector to coefficients of the basis functions of boundary
+    up # Projection from coefficients of the basis functions of boundary to force vector
+    R # Some Function that holds the regularizer, a convex lower-semicontinuous function
+    β::Float64 # Regularization parameter
+    τ::Float64 # Learn rate
+    num_pairs::Int64 # Number of voltage-current pairs
+
 
 end
