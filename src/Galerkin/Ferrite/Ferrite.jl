9c34018cd3cfe71942b2ce77ad95e2660765c458
DanielBoigk <dn.boigk@gmail.com>
2025-10-03 00:00:22 +0200

    fix names

--- src/Galerkin/Ferrite/Ferrite.jl
+++ src/Galerkin/Ferrite/Ferrite.jl
@@ -1,2 +1,4 @@
 include("FESpace.jl")
 include("Assemblers/MatrixAssemblers.jl")
+include("Assemblers/RHSAssemblers.jl")
+include("Assemblers/TensorAssembler.jl")
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using ModularEIT
 using Test
 include("AssemblerTests/BilinearMap.jl")
+include("AssemblerTests/MatrixTests.jl")
 @testset "ModularEIT.jl" begin
     # Write your tests here.
 end
